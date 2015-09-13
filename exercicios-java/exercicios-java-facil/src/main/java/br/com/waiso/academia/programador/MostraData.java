db94de98c3b90cae7011a9492cc6c9e925bf5de4
fabianormatias <fabianormatias@gmail.com>
2015-09-13 17:53:37 -0400

    add exercicios da academia do programador

---
+++ exercicios-java/exercicios-java-facil/src/main/java/br/com/waiso/academia/programador/MostraData.java
@@ -0,0 +1,21 @@
+package br.com.waiso.academia.programador;
+/*
+ * Programa Java demonstra como recuperar a data atual (máquina).
+ *
+ * Módulo: Lógica de programação
+ * Author: Eder Magalhães
+ */
+
+import java.util.Date;
+
+public class MostraData {
+
+	public static void main(String[] args) {
+		Date hoje = new Date();
+
+		System.out.printf("Data (original): %s %n", hoje);
+		System.out.printf("Data formatada: %1$te/%1$tm/%1$tY %n", hoje);
+		System.out.printf("Data outro formato: %tD %n", hoje);
+	}
+
+}
